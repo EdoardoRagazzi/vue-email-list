c68701371d826a5769091b7454f6f19833249fc3
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-02 14:31:53 +0200

    Array

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ const app = new Vue(
         el: "#app",
         data: {
             number: null,
+            array: []
         },
         mounted() {
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(result => {
-                    this.number = result.data.response;
-                    console.log(this.number)
-                });
+            for (let i = 0; i < 10; i++)
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail ')
+                    .then(result => {
+                        this.number = result.data.response;
+                        this.array.push(this.number)
+                        console.log(this.number)
+                    });
         }
 
     }
